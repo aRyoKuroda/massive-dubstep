0a7383d1a325ab011ac61625816da59079d83384
Ryo Kajiwara <ryo.kajiwara@access-company.com>
2014-04-28 14:59:36 +0900

    schema.rb update

--- StackUnderflow/db/schema.rb
+++ StackUnderflow/db/schema.rb
@@ -13,6 +13,30 @@
 
 ActiveRecord::Schema.define(version: 20140428052729) do
 
+  create_table "answer_comments", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "answer_votes", force: true do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "answers", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "question_comments", force: true do |t|
     t.integer  "question_id"
     t.integer  "user_id"
@@ -30,11 +54,29 @@ ActiveRecord::Schema.define(version: 20140428052729) do
   end
 
   create_table "questions", force: true do |t|
-    t.string   "name"
-    t.string   "user_id"
-    t.text     "text"
+    t.string   "title"
+    t.integer  "user_id"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
