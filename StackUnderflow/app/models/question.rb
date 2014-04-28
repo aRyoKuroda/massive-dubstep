c412f2e03ca7fd2cc49ee994dc6f7cfdc45945e4
Ryo Kajiwara <ryo.kajiwara@access-company.com>
2014-04-28 15:07:06 +0900

    set relations to questions

--- StackUnderflow/app/models/question.rb
+++ StackUnderflow/app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+  has_many :question_votes
+  has_many :question_comments
 end
--- StackUnderflow/app/models/question_comment.rb
+++ StackUnderflow/app/models/question_comment.rb
@@ -1,2 +1,3 @@
 class QuestionComment < ActiveRecord::Base
+  belongs_to :question
 end
